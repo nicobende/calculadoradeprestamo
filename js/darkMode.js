c0dbc676cbdfd054da96c7ca48eb174b7f4620a2
nicobende <niko_bendele@hotmail.com>
2024-01-28 19:42:22 -0300

    Se modifica darkMode y css

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,54 +1,13 @@
 
-let darkMode;
+const colorModeButton = document.querySelector("#color-mode");
+const body = document.body;
 
-if (localStorage.getItem("blackMode")) {
-    darkMode = localStorage.getItem("blackMode");
-} else {
-    darkMode = "light";
-}
-
-
-localStorage.setItem("blackMode", darkMode);
-
-
-
-$( () => {
-    if (localStorage.getItem("blackMode") == 'dark') {
-        $("body").addClass("blackMode");
-        $("#btnDarkMode").hide();
-        $("#btnLightMode").show();
+colorModeButton.addEventListener("click", cambiarModoColor);
+function cambiarModoColor() {
+    body.classList.toggle("dark-mode");
+    if (body.classList.contains("dark-mode")) {
+        colorModeButton.innerText = "Cambiar a Light Mode";
     } else {
-        $("#btnLightMode").hide();
+        colorModeButton.innerText = "Cambiar a Dark Mode";
     }
-
-    $("#btnLightMode").click( () => {
-        $("#btnLightMode").hide();
-        $("#btnDarkMode").show();
-
-
-        $("#navbar-toggle").removeClass("navbar-dark bg-dark");
-        $("#navbar-toggle").addClass("navbar-light bg-light");
-        
-        $("#mostrar-creditos").removeClass("table-dark");
-        $("#mostrar-creditos").addClass("table-light");
-
-
-        $("body").removeClass("blackMode");
-        localStorage.setItem("blackMode", "light")
-    })
-
-
-    $("#btnDarkMode").click( () => {
-        $("#btnLightMode").show();
-        $("#btnDarkMode").hide();
-
-        $("#navbar-toggle").removeClass("navbar-light bg-light");
-        $("#navbar-toggle").addClass("navbar-dark bg-dark");
-        
-        $("#mostrar-creditos").removeClass("table-light");
-        $("#mostrar-creditos").addClass("table-dark");
-        
-        $("body").addClass("blackMode");
-        localStorage.setItem("blackMode", "dark")
-    })
-})
+}
