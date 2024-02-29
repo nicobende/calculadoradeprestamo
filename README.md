d2539a54986887487338c1f8ffd4e2da75d32860
nicobende <niko_bendele@hotmail.com>
2024-02-28 23:35:43 -0300

    Para entrega final

---
+++ README.md
@@ -0,0 +1,16 @@
+Calculadora de Préstamos
+Este es un proyecto de calculadora de préstamos que permite a los usuarios calcular el monto del préstamo, la tasa de interés y el plazo del préstamo en meses. Además, los usuarios pueden enviar una solicitud de préstamo ingresando su nombre, apellido y correo electrónico.
+
+Se agrega una funcion "dark mode" con un boton en la parte superior "Cambiar a Dark Mode" que cambia color de pagina de clara a oscura.
+
+En ste código se utiliza fetch() para obtener datos de una API, maneja la respuesta y los posibles errores, y actualiza el DOM con la información obtenida.
+
+Tecnologías Utilizadas
+HTML
+CSS (Bootstrap)
+JavaScript
+
+Cómo Funciona
+El usuario ingresa los datos del préstamo en la sección "Calculadora de Préstamo" y hace clic en el botón "Calcular". Los resultados del préstamo se muestran en la página.
+
+Para enviar una solicitud de préstamo, el usuario ingresa su nombre, apellido y correo electrónico en la sección "Solicitud de Préstamo" y hace clic en el botón "Solicitar Préstamo". Una vez enviado, se muestra un mensaje de confirmación en la página.
--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
+
+// doolar hoy de dolar API
+document.addEventListener('DOMContentLoaded', function() {
+    obtenerValorDolar();
+});
+
+function obtenerValorDolar() {
+    fetch("https://dolarapi.com/v1/dolares/oficial")
+        .then(function(response) {
+            if (!response.ok) {
+                throw new Error('No se pudo obtener el valor del dólar');
+            }
+            return response.json();
+        })
+        .then(function(data) {
+            // JSON en la consola
+            console.log("Datos del dólar:", data);
+
+            // compra y venta
+            const valorCompra = data.compra;
+            const valorVenta = data.venta;
+            
+            // valores en el DOM
+            const dolarContainer = document.getElementById("dolar-container");
+            dolarContainer.innerHTML = "<p>Compra: $" + valorCompra + "</p><p>Venta: $" + valorVenta + "</p>";
+            
+        })
+        .catch(function(error) {
+            console.error("Error al obtener el valor del dólar:", error);
+        });
+}
+
+// tipo "carrito" donde se ven los prestamos calculados
+
+let carritoPrestamos = [];
+
 function calcularPrestamo() {
     const monto = parseFloat(document.getElementById("monto").value);
     const tasaInteres = parseFloat(document.getElementById("tasaInteres").value);
@@ -7,8 +43,6 @@ function calcularPrestamo() {
     const cuotaMensual = monto * tasaInteresMensual / (1 - (1 + tasaInteresMensual) ** -plazo);
     const pagoTotal = cuotaMensual * plazo;
 
-    const prestamoArray = [];
-
     const cuotaPrestamo = {
         cuotaMensual: cuotaMensual.toFixed(2),
         pagoTotal: pagoTotal.toFixed(2),
@@ -17,29 +51,38 @@ function calcularPrestamo() {
         plazo: plazo
     };
 
-    // Uso de push
-    prestamoArray.push(cuotaPrestamo);
+    carritoPrestamos.push(cuotaPrestamo);
 
-    // DOM
-    const pagoMensual = document.getElementById("pagoMensual");
-    const montoTotal = document.getElementById("montoTotal");
+    actualizarCarrito();
 
-    pagoMensual.innerText = `Pago Mensual: $${cuotaMensual.toFixed(2)}`;
-    montoTotal.innerText = `Pago Total: $${pagoTotal.toFixed(2)}`;
+    // localStorage
+    guardarEnLocalStorage("carritoPrestamos", JSON.stringify(carritoPrestamos));
+    
+    return cuotaPrestamo;
+}
 
-    // LocalStorage
-    guardarEnLocalStorage("cuotaMensual", cuotaMensual.toFixed(2));
-    guardarEnLocalStorage("pagoTotal", pagoTotal.toFixed(2));
-    guardarEnLocalStorage("prestamoArray", JSON.stringify(prestamoArray));
+function actualizarCarrito() {
+    const carritoContainer = document.getElementById("carritoContainer");
+    carritoContainer.innerHTML = ''; 
 
-    // Uso de filter 
-    const cuotasFiltradas = prestamoArray.filter(cuota => cuota.cuotaMensual > 100);
+    carritoPrestamos.forEach((prestamo, index) => {
+        const prestamoElement = document.createElement("div");
+        prestamoElement.innerHTML = `
+            <p>Monto: $${prestamo.monto}</p>
+            <p>Tasa de Interés: ${prestamo.tasaInteres}%</p>
+            <p>Plazo: ${prestamo.plazo} meses</p>
+            <p>Cuota Mensual: $${prestamo.cuotaMensual}</p>
+            <p>Pago Total: $${prestamo.pagoTotal}</p>
+            <button onclick="eliminarPrestamo(${index})" class="btn btn-info btn-lg btn-block">Eliminar</button>
+        `;
+        carritoContainer.appendChild(prestamoElement);
+    });
+}
 
-    // uso de map 
-    const montosArray = prestamoArray.map(cuota => cuota.monto);
-
-    console.log(cuotasFiltradas);
-    console.log(montosArray);
+function eliminarPrestamo(index) {
+    carritoPrestamos.splice(index, 1);
+    actualizarCarrito();
+    guardarEnLocalStorage("carritoPrestamos", JSON.stringify(carritoPrestamos));
 }
 
 function guardarEnLocalStorage(clave, valor) {
@@ -49,3 +92,66 @@ function guardarEnLocalStorage(clave, valor) {
         console.error("El navegador no soporta localStorage.");
     }
 }
+
+// recuperar el carrito del localStorage
+window.onload = function() {
+    const carritoGuardado = localStorage.getItem("carritoPrestamos");
+    if (carritoGuardado) {
+        carritoPrestamos = JSON.parse(carritoGuardado);
+        actualizarCarrito();
+    }
+};
+
+//solicitud prestamos
+
+let solicitudesPrestamo = []; 
+
+document.addEventListener('DOMContentLoaded', function() {
+  const solicitudForm = document.getElementById('solicitudForm');
+  const mensajeSolicitud = document.getElementById('mensajeSolicitud');
+
+  solicitudForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const email = document.getElementById('email').value;
+    const monto = document.getElementById('monto').value; 
+
+    solicitarPrestamo(nombre, apellido, email, monto);
+
+
+
+  });
+
+  function solicitarPrestamo(nombre, apellido, email, monto) {
+    const solicitud = {
+      nombre: nombre,
+      apellido: apellido,
+      email: email,
+      monto: monto,
+    };
+
+    
+    solicitudesPrestamo.push(solicitud);
+
+    // mensaje de pedido
+    mensajeSolicitud.textContent = `Su pedido de: $${monto} está en proceso, nos comunicaremos con usted.`;
+    mensajeSolicitud.style.display = "block";
+
+    // Guardar formato JSON
+    guardarSolicitudesJSON();
+  }
+
+  function guardarSolicitudesJSON() {
+    // solicitudes a JSON
+    const solicitudesJSON = JSON.stringify(solicitudesPrestamo);
+
+    // JSON en el localStor
+    localStorage.setItem('solicitudesPrestamo', solicitudesJSON);
+  }
+
+});
+
+  
+
